e118521fa159633824291eb20d0d03072569b22b
Wyatt <wyattlefevre@gmail.com>
2023-01-18 15:48:55 -0700

    latest

--- .config/nvim/lua/user/autotag.lua
+++
@@ -1,6 +0,0 @@
-local status_ok, ntag = pcall(require, "nvim-ts-autotag")
-if not status_ok then
-  return
-end
-
-ntag.setup()
--- .zsh/aliases/docker.sh
+++
@@ -1,5 +0,0 @@
-# docker
-alias du='docker compose up'
-alias dbu='docker compose up --build'
-alias dd='docker compose down'
-alias dr='docker compose run --rm'
