6fdb3322b610a4f2a7c809977ba260ad461923af
Wyatt <wyattlefevre@gmail.com>
2023-01-26 10:56:08 -0700

    latest changes from mac

--- .zsh/aliases/macbook.sh
+++
@@ -1,8 +0,0 @@
-function mobile() {
-  xrdb -load ~/.Xresources.mobile
-  echo 'awesome.restart()' | awesome-client
-}
-function docked() {
-  xrdb -load ~/.Xresources
-  echo 'awesome.restart()' | awesome-client
-}
